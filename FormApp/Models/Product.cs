ec99f057c13a422057c95caeb8f4885529417612
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-05-26 00:48:03 +0300

    PrpductList

---
+++ FormApp/Models/Product.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormApp.Models
+{
+    public class Product
+    {
+        [Display(Name="Urun Id")]
+        public int ProductId { get; set; }
+        [Display(Name="Urun Adı")]
+        public string Name { get; set; }=string.Empty;
+        [Display(Name="Fiyat")]
+        public decimal Price { get; set; }
+        [Display(Name="Resim")]
+        public string Image { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
