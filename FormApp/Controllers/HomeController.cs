6074be37cf71b9b17d5cb997613a3a8db077e2bf
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-06-15 14:03:59 +0300

    GetFormu

--- FormApp/Controllers/HomeController.cs
+++ FormApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormApp.Controllers;
 
@@ -13,9 +14,25 @@ public class HomeController : Controller
        
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string searchString , string category)
     {
-        return View(Repository.Products);
+        var products= Repository.Products;
+        //ısnullorempty bize null sa true döner o yüzden biz degilini aldık.Başa ! koyduk
+        if(!string.IsNullOrEmpty(searchString))
+        {
+
+            ViewBag.searchString=searchString;
+            // contains bir öğeyi içerip içermediğini kontrol etmek için kullanılan bir metottur.
+           products = products.Where(p => p.Name.ToUpper().Contains(searchString.ToUpper())).ToList();
+        }
+
+        if(!string.IsNullOrEmpty(category) && category!="0")
+        {
+            products=products.Where(p=>p.CategoryId==int.Parse(category)).ToList();
+        }
+
+        ViewBag.Categories= new SelectList(Repository.Categories, "CategoryId" , "Name" , category);
+        return View(products);
     }
 
     public IActionResult Privacy()
--- FormApp/Models/Repository.cs
+++ FormApp/Models/Repository.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 
 namespace FormApp.Models
 {
+
+    // Şimdilik veri tabanı yerine 
     public class Repository
     {
+        //readonly sadece okunur olur
         private static readonly List<Product> _products=new();
         private static readonly List<Category> _categories=new();
        
@@ -14,13 +17,13 @@ namespace FormApp.Models
         _categories.Add(new Category{CategoryId=1, Name="Telefon"});
         _categories.Add(new Category{CategoryId=2, Name="Bilgisayar"});
 
-        _products.Add(new Product{ProductId=1,Name="IPhone 18",Price=20000, IsActive=true, Image="1.jpg", CategoryId=1});
-        _products.Add(new Product{ProductId=2,Name="IPhone 19",Price=30000, IsActive=true, Image="2.jpg", CategoryId=1});
-        _products.Add(new Product{ProductId=3,Name="IPhone 20",Price=40000, IsActive=true, Image="3.jpg", CategoryId=1});
-        _products.Add(new Product{ProductId=3,Name="IPhone 30",Price=50000, IsActive=true, Image="4.jpg", CategoryId=1});
+         _products.Add(new Product {ProductId = 1, Name = "Samsung 14", Price = 40000, IsActive = true,Image = "1.jpg",CategoryId = 1 });
+        _products.Add(new Product {ProductId = 2, Name = "Samsung 15", Price = 50000, IsActive = false,Image = "2.jpg",CategoryId = 1 });
+         _products.Add(new Product {ProductId = 3, Name = "Samsung 16", Price = 60000, IsActive = true,Image = "3.jpg",CategoryId = 1 });
+         _products.Add(new Product {ProductId = 4, Name = "Xiaomi 14", Price = 70000, IsActive = false,Image = "4.jpg",CategoryId = 1 });
 
-        _products.Add(new Product{ProductId=4,Name="Lenova",Price=80000, IsActive=true, Image="5.jpg", CategoryId=2});
-        _products.Add(new Product{ProductId=5,Name="Macbook Pro",Price=50000, IsActive=true, Image="6.jpg", CategoryId=2});
+        _products.Add(new Product{ProductId=5,Name="Lenova",Price=80000, IsActive=true, Image="5.jpg", CategoryId=2});
+        _products.Add(new Product{ProductId=6,Name="Macbook Pro",Price=50000, IsActive=true, Image="6.jpg", CategoryId=2});
 
        }
         public static List<Product> Products{
