00c6541cbf2ede720dc4f240e4db2106e75a9191
Denidalan <125769544+Denidalan@users.noreply.github.com>
2024-08-17 01:24:12 +0300

    IdentityApp

---
+++ IdentityApp/Models/IdentitySeedData.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context=app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if(context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            var userManager=app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var user=await userManager.FindByNameAsync(adminUser);
+
+            if(user==null)
+            {
+                user=new IdentityUser {
+                    UserName=adminUser,
+                    Email="dnz@info.com",
+                    PhoneNumber="1111111"
+                };
+
+                await userManager.CreateAsync(user , adminPassword);
+            }
+        }
+    }
+}
